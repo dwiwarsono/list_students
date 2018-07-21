eb563683a6feb02b761f5019efe95d26b3359327
thebriv057 <dwi2warsono@gmail.com>
2018-07-21 13:41:54 +0700

    Penambahan ListSiswa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './Header';
-import FormSiswa from './FormSiswa';
+import FormSiswa from './siswa/FormSiswa';
 
 class App extends Component {
   render() {
